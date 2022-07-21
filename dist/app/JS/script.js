f6c27a59145569cdf0fe905d4f15cfbc7e122c5f
RenanFerreira0412 <renan_igor03@hotmail.com>
2022-07-21 19:29:14 -0300

    Implementação do login com o google imcompleto

--- dist/app/JS/script.js
+++ dist/app/JS/script.js
@@ -374,6 +374,35 @@ $(document).ready(function () {
                 }
             },
 
+            googleLogin: function () {
+                var provider = new firebase.auth.GoogleAuthProvider();
+
+                authRef
+                    .signInWithPopup(provider)
+                    .then((result) => {
+                        ///** @type {firebase.auth.OAuthCredential} */
+                        var credential = result.credential;
+
+                        // This gives you a Google Access Token. You can use it to access the Google API.
+                        var token = credential.accessToken;
+                        // The signed-in user info.
+                        var user = result.user;
+                        // ...
+                    }).catch((error) => {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorCode)
+                        console.log(errorMessage)
+                        // The email of the user's account used.
+                        var email = error.email;
+                        // The firebase.auth.AuthCredential type that was used.
+                        var credential = error.credential;
+                        // ...
+                    });
+
+            },
+
             verificaSignInErros: function (errorMessage) {
                 switch (errorMessage) {
                     case 'auth/user-not-found':
